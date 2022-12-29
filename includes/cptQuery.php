cb1e92cbdbde7f248cfbf74b8bb586166c8cb556
WayneGao <wckao0513@iwangoweb.com>
2022-12-29 09:45:00 +0800

    fix : 修改模糊比對時，儲存陣列邏輯 : index -> push

--- includes/cptQuery.php
+++ includes/cptQuery.php
@@ -1,18 +1,17 @@
 <?php
 
 /*搜尋文章分類下的店家資訊*/
-function cptQuery( $cptSlug, $taxSlug, $cptTaxSlug, $post_per_page, $mode ) {
+function cptQuery($cptSlug, $taxSlug, $cptTaxSlug, $post_per_page, $mode)
+{
 
-	switch ( $mode ) {
+	switch ($mode) {
 		case "part":
 			$content = [[[]]]; //儲存前端呈現資料
 			$city = ''; //儲存有資料的城市
 			$exist_company = []; //儲存已存在公司，用以比對
-			foreach ($taxSlug as $value)
-			{
-				$splitArray = explode('-',$value); //以"-"分割，如：txg-land-second-mortgage  [0]->txg [1]->land ...
+			foreach ($taxSlug as $value) {
+				$splitArray = explode('-', $value); //以"-"分割，如：txg-land-second-mortgage  [0]->txg [1]->land ...
 				$city = $splitArray[0]; //僅取得城市分類的slug名稱
-				$i = 0;
 				$query_string = array(
 					'post_type' => $cptSlug,  //cpt slug的名稱
 					$cptTaxSlug => $value,  //tax slug名稱
@@ -21,16 +20,17 @@ function cptQuery( $cptSlug, $taxSlug, $cptTaxSlug, $post_per_page, $mode ) {
 				$queryCategory = new WP_Query($query_string); //搜尋分類內容
 
 				//存在post時執行
-				if ($queryCategory -> have_posts()) {
-					while ($queryCategory -> have_posts()) {
-						$queryCategory -> the_post(); //指向下一篇
+				if ($queryCategory->have_posts()) {
+					while ($queryCategory->have_posts()) {
+						$queryCategory->the_post(); //指向下一篇
 						$title = get_the_title();
-						if ( in_array( $city . $title, $exist_company ) == False ){
-							$content[$city][$i]['featuredImg'] = get_the_post_thumbnail_url(get_the_ID(), 'full');  //取得特色圖片
-							$content[$city][$i]['title'] = $title; //取得標題
-							$content[$city][$i]['link'] = get_the_permalink(); //取得網址
-							$i++;
-							array_push( $exist_company, $city . $title );
+						if (in_array($city . $title, $exist_company) == False) {
+							$content[$city][] = array(
+								'featuredImg' => get_the_post_thumbnail_url(get_the_ID(), 'full'), //取得特色圖片
+								'title' => $title, //取得標題
+								'link' => get_the_permalink() //取得網址
+							);
+							array_push($exist_company, $city . $title);
 						}
 					}
 				}
@@ -41,21 +41,20 @@ function cptQuery( $cptSlug, $taxSlug, $cptTaxSlug, $post_per_page, $mode ) {
 		case "full":
 			$content = [[]]; //放內容標題、圖片網址、連結
 			/*Taxonomy Slug若為空值，不執行Query Loop*/
-			if ( !isset($taxSlug) ) {
+			if (!isset($taxSlug)) {
 				return;
 			}
 			$query_string = array(
 				'post_type' => $cptSlug,  //cpt slug的名稱
 				$cptTaxSlug => $taxSlug,  //tax slug名稱
-				'posts_per_page' => $post_per_page, 
+				'posts_per_page' => $post_per_page,
 			);
 
 			$queryCategory = new WP_Query($query_string); //搜尋分類內容
-			$j=0;
-			if ($queryCategory -> have_posts()) {
-				while ($queryCategory -> have_posts())
-				{
-					$queryCategory -> the_post(); //指向下一篇
+			$j = 0;
+			if ($queryCategory->have_posts()) {
+				while ($queryCategory->have_posts()) {
+					$queryCategory->the_post(); //指向下一篇
 					$content[$j]['featuredImg'] = get_the_post_thumbnail_url(get_the_ID(), 'full');  //取得特色圖片
 					$content[$j]['title'] = get_the_title(); //取得標題
 					$content[$j]['link'] = get_the_permalink(); //取得網址
@@ -67,5 +66,3 @@ function cptQuery( $cptSlug, $taxSlug, $cptTaxSlug, $post_per_page, $mode ) {
 			break;
 	}
 }
-
-?>
--- wg-custom-posts.php
+++ wg-custom-posts.php
@@ -3,7 +3,7 @@
 Plugin Name: WG Custom Posts
 Plugin URI: https://www.iwangoweb.com/
 Description: 客製化版型Query Posts，包裝成帶有參數之短碼。
-Version: 1.2.3
+Version: 1.2.4
 Author: 玩構網路
 Author URI: https://www.iwangoweb.com/about/
 */
@@ -17,6 +17,7 @@ Author URI: https://www.iwangoweb.com/about/
 //v1.2.1 20221006 修改CSS樣式
 //v1.2.2 20221101 屏東市 -> 屏東縣
 //v1.2.3 20221108 Query參數 post_per_page -> posts_per_page
+//v1.2.4 20221229 修改模糊比對時，儲存陣列邏輯 : index -> push
 //一共5個function  分別用途：1.主程式整合 2.(後端)使用者輸入name轉成slug 3.(後端)轉換後的slug進行搜尋商家 4.(前端)顯示商家資訊 5.引入外部CSS檔
 /*整合用主程式*/
 
